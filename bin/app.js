7543816ab9cb1298110d363774dc2ca4d28a844b
James McGill <jmcgill@Jamess-iMac.home>
2017-09-03 13:37:44 -0400

    Reformat with prettier

--- bin/app.js
+++ bin/app.js
@@ -23,92 +23,96 @@ var header = `
      $/__/         $/__/         $/__/       $/__/         $/__/         $/__/    
 `;
 
-console.log(chalk.blue(header.replace(/\$/ig, '\\')));
+console.log(chalk.blue(header.replace(/\$/gi, '\\')));
 
 function createDirectoryIfMissing(path) {
-    if (!fs.existsSync(path)){
-        fs.mkdirSync(path);
-    }
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
 }
 
 // TODO(jmcgill): Move working path to application state.
 function initialize() {
-    createDirectoryIfMissing(path.join(os.homedir(), '.slider'));
+  createDirectoryIfMissing(path.join(os.homedir(), '.slider'));
 }
 
 function printDiff(preamble, diff) {
-    var parts = diff.split('\n');
-    for (var part of parts) {
-        if (part.startsWith('+++')) {
-            console.log(preamble, chalk.green(part));
-        } else if (part.startsWith('---')) {
-            console.log(preamble, chalk.red(part));
-        } else {
-            console.log(preamble, part)
-        }
+  var parts = diff.split('\n');
+  for (var part of parts) {
+    if (part.startsWith('+++')) {
+      console.log(preamble, chalk.green(part));
+    } else if (part.startsWith('---')) {
+      console.log(preamble, chalk.red(part));
+    } else {
+      console.log(preamble, part);
     }
+  }
 }
 
 var config = {
-    options: {
-        id: 'test12'
-    },
-    fn: function ls(root) {
-        log.info('Running ls function on: ', root);
-        // Create a new owners file
-        fs.writeFileSync(path.join(root, 'JAMES'), 'this is a new file');
+  options: {
+    id: 'test12',
+  },
+  fn: function ls(root) {
+    log.info('Running ls function on: ', root);
+    // Create a new owners file
+    fs.writeFileSync(path.join(root, 'JAMES'), 'this is a new file');
 
-        return 5;
-    },
-    reviewers: function(root) {
-        return ["plexer"]
-    }
+    return 5;
+  },
+  reviewers: function(root) {
+    return ['plexer'];
+  },
 };
 
 (async function main() {
-    initialize();
+  initialize();
 
-    commander.version('0.1.0')
-        .option('-e, --expression', 'Expression')
-        .option('-d, --dryrun', 'Dry run')
-        .parse(process.argv);
+  commander
+    .version('0.1.0')
+    .option('-e, --expression', 'Expression')
+    .option('-d, --dryrun', 'Dry run')
+    .parse(process.argv);
 
-    // TODO(jimmy): Enable Dry Runs.
-    const walker = new GithubWalker(/short/, path.join(os.homedir(), '.slider'), config);
-    var r = await walker.walk({
-        user: "jmcgill"
-    });
+  // TODO(jimmy): Enable Dry Runs.
+  const walker = new GithubWalker(
+    /short/,
+    path.join(os.homedir(), '.slider'),
+    config
+  );
+  var r = await walker.walk({
+    user: 'jmcgill',
+  });
 
-    console.log('CALLING THEN WITH ', r);
+  console.log('CALLING THEN WITH ', r);
 
-    for (var url in r) {
-        console.log(chalk.blue.bold(url));
-        for (var diff in r[url].status) {
-            console.log('    ', chalk.magenta.bold(diff));
-            printDiff('       ', r[url].status[diff]);
-        }
-        console.log('\n');
-        console.log('    ', chalk.magenta.bold('Slider Output'));
-        console.log(indentString(JSON.stringify(r[url].result), 8));
-        console.log('\n');
+  for (var url in r) {
+    console.log(chalk.blue.bold(url));
+    for (var diff in r[url].status) {
+      console.log('    ', chalk.magenta.bold(diff));
+      printDiff('       ', r[url].status[diff]);
+    }
+    console.log('\n');
+    console.log('    ', chalk.magenta.bold('Slider Output'));
+    console.log(indentString(JSON.stringify(r[url].result), 8));
+    console.log('\n');
+  }
+
+  // Print status table
+  var rows = [];
+  for (var url in r) {
+    var status = chalk.green('COMPLETE');
+    if (r[url].pullRequest) {
+      status = chalk.red('PENDING REVIEW');
     }
 
-
-    // Print status table
-    var rows = [];
-    for (var url in r) {
-        var status = chalk.green('COMPLETE');
-        if (r[url].pullRequest) {
-            status = chalk.red('PENDING REVIEW');
-        }
-
-        var pullRequestUrl = '';
-        if (r[url].pullRequest) {
-            pullRequestUrl = r[url].pullRequest.url;
-        }
-
-        rows.push([chalk.blue(url), status, pullRequestUrl]);
+    var pullRequestUrl = '';
+    if (r[url].pullRequest) {
+      pullRequestUrl = r[url].pullRequest.url;
     }
 
-    console.log(table(rows));
+    rows.push([chalk.blue(url), status, pullRequestUrl]);
+  }
+
+  console.log(table(rows));
 })();
